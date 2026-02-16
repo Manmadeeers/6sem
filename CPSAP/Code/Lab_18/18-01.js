e5839bd66476cf154895b62a508cc94dc92dc169
Manmad <philmade6@gmail.com>
2026-02-16 10:47:41 +0300

    CPSAP:lab18 in progress. PUT endpoints in progress

--- CPSAP/Code/Lab_18/18-01.js
+++ CPSAP/Code/Lab_18/18-01.js
@@ -3,6 +3,9 @@ const seq = require('sequelize');
 const ted = require('tedious');
 const fs = require('fs');
 const path = require('path');
+const { type } = require('os');
+const { stringify } = require('querystring');
+const { error } = require('console');
 
 const PORT = 3000;
 
@@ -10,21 +13,34 @@ const app = express();
 const sequelize = new seq.Sequelize(
     'UNIVER',
     'sa',
-    '123StrongPass!',{
-        dialect:'mssql',
-        host:'localhost'
-    }
+    '123StrongPass!', {
+    dialect: 'mssql',
+    host: 'localhost'
+}
 );
+
+app.use(express.json());
+
+const PayloadChecker = (payload, res) => {
+    if (!payload || typeof (payload) != 'object') {
+        return res.status(400).json({ error: "Bad request. Payload was invalid" });
+    }
+}
+
+const ErrorHandler = (err, res, table, method) => {
+    console.error(`${table} ${method} ERROR: `, err);
+    return res.status(500).json({ error: `Internal server error: ${err}` });
+}
 
 
 sequelize.authenticate()
-.then(()=>{
-    console.log("Database connaction established");
+    .then(() => {
+        console.log("Database connaction established");
 
-})
-.catch(err=>{
-    console.log("Database connection failed. ",err);
-});
+    })
+    .catch(err => {
+        console.log("Database connection failed. ", err);
+    });
 
 const orm = require('./ORM_helper').ORM(sequelize);
 
@@ -44,76 +60,346 @@ app.get('/', async (req, res) => {
         });
     }
     catch (err) {
-        console.error(err);
+        ErrorHandler(err, res, '', 'GET');
     }
 
 });
 
-app.get('/api/faculties',async(req,res)=>{
-    try{
+app.get('/api/faculties', async (req, res) => {
+    try {
         orm.Faculty.findAll()
-        .then(faculties=>{return res.status(200).json(faculties)})
-        .catch(err=>{return res.status(500).json({error:`Something went wrong: ${err}`})});
+            .then(faculties => { return res.status(200).json(faculties) })
+            .catch(err => { throw new Error(err) });
     }
-    catch(err){
-        console.error(err);
+    catch (err) {
+        ErrorHandler(err, res, "FACULTIES", 'GET');
     }
 });
 
-app.get('/api/pulpits',async(req,res)=>{
-    try{
+app.get('/api/pulpits', async (req, res) => {
+    try {
         orm.Pulpit.findAll()
-        .then(pulpits=>{return res.status(200).json(pulpits)})
-        .catch(err=>{res.status(500).json({error:`Something went wrong: ${err}`})});
+            .then(pulpits => { return res.status(200).json(pulpits) })
+            .catch(err => { throw new Error(err) });
     }
-    catch(err){
-        console.error(err);
+    catch (err) {
+        ErrorHandler(err, res, "PULPITS", 'GET');
     }
 });
 
-app.get('/api/subjects',async(req,res)=>{
-    try{
+app.get('/api/subjects', async (req, res) => {
+    try {
         orm.Subject.findAll()
-        .then(subjects=>{return res.status(200).json(subjects)})
-        .catch(err=>{return res.status(500).json({error:`Something went wrong: ${err}`})});
+            .then(subjects => { return res.status(200).json(subjects) })
+            .catch(err => { throw new Error(err) });
     }
-    catch(err){
-        console.error(err);
+    catch (err) {
+        ErrorHandler(err, res, "SUBJECTS", 'GET');
     }
 });
 
-app.get('api/auditoriumstypes',async(req,res)=>{
-    try{
+app.get('/api/auditoriumstypes', async (req, res) => {
+    try {
         orm.Auditorium_type.findAll()
-        .then(auditoriumstypes=>{return res.status(200).json(auditoriumstypes)})
-        .catch(err=>{return res.status(500).json({error:`Something went wrong: ${err}`})});
+            .then(auditoriumstypes => { return res.status(200).json(auditoriumstypes) })
+            .catch(err => { throw new Error(err) });
     }
-    catch(err){
-        console.error(err);
+    catch (err) {
+        ErrorHandler(err, res, "AUDITORIUM_TYPES", "GET");
     }
 });
 
-app.get('api/auditoriums',async(req,res)=>{
-    try{
+app.get('/api/auditoriums', async (req, res) => {
+    try {
         orm.Auditorium.findAll()
-        .then(auditoriums=>{return res.status(200).json(auditoriums)})
-        .catch(err=>{return res.status(500).json({error:`Something went wrong: ${err}`})});
+            .then(auditoriums => { return res.status(200).json(auditoriums) })
+            .catch(err => { throw new Error(err) });
     }
-    catch(err){
-        console.error(err);
+    catch (err) {
+        ErrorHandler(err, res, "AUDITORIUM", "GET");
     }
 });
 
 
-app.post('/api/faculties',async(req,res)=>{
+app.post('/api/faculties', async (req, res) => {
+    try {
+        const payload = req.body;
+        console.log(payload);
+
+        PayloadChecker(payload, res);
+
+        const { faculty, faculty_name } = payload;
+        if (!faculty || typeof (faculty) != 'string' || !faculty_name || typeof (faculty_name) != 'string') {
+            return res.status(400).json({ error: "Bad request. Payload fields were invalid" });
+        }
+
+        orm.Faculty.create({ faculty: faculty, faculty_name: faculty_name })
+            .then(task => {
+                console.log(task.dataValues);
+                return res.status(201).json(task.dataValues);
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+    }
+    catch (err) {
+        ErrorHandler(err, res, "FACULTIES", "POST");
+    }
+});
+
+
+app.post('/api/pulpits', async (req, res) => {
+    try {
+        const payload = req.body;
+        console.log(payload);
+
+        PayloadChecker(payload, res);
+
+        const { pulpit, pulpit_name, faculty } = payload;
+        if (!pulpit || typeof (pulpit) != 'string' || !pulpit_name || typeof (pulpit_name) != 'string' || !faculty || typeof (faculty) != 'string') {
+            return res.status(400).json({ error: "Bad request. Payload fields were invalid" });
+        }
+
+
+        orm.Pulpit.create({ pulpit: pulpit, pulpit_name: pulpit_name, faculty: faculty })
+            .then(task => {
+                console.log(task.dataValues);
+                return res.status(201).json(task.dataValues);
+            })
+            .catch(err => {
+                throw new Error(err);
+            })
+    }
+    catch (err) {
+        ErrorHandler(err, res, "PULPITS", "POST");
+    }
+});
+
+app.post('/api/subjects', async (req, res) => {
+    try {
+        const payload = req.body;
+
+        PayloadChecker(payload, res);
+
+        const { subject, subject_name, pulpit } = payload;
+
+        if (!subject || typeof (subject) != 'string' || !subject_name || typeof (subject_name) != 'string' || !pulpit || typeof (pulpit) != 'string') {
+            return res.status(400).json({ error: "Bad request. Payload fields were invalid" });
+        }
+
+        orm.Subject.create({ subject: subject, subject_name: subject_name, pulpit: pulpit })
+            .then(task => {
+                console.log(task.dataValues);
+                return res.status(201).json(task.dataValues);
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+    }
+    catch (err) {
+        ErrorHandler(err, res, "SUBJECTS", "POST");
+    }
+});
+
+app.post('/api/auditoriumstypes', async (req, res) => {
+    try {
+        const payload = req.body;
+
+        PayloadChecker(payload, res);
+
+        const { auditorium_type, auditorium_type_name } = payload;
+        if (!auditorium_type || typeof (auditorium_type) != 'string' || !auditorium_type_name || typeof (auditorium_type_name) != 'string') {
+            return res.status(400).json({ error: "Bad request. Payload fields were invalid" });
+        }
+
+        orm.Auditorium_type.create({ auditorium_type: auditorium_type, auditorium_type_name: auditorium_type_name })
+            .then(task => {
+                console.log(task.dataValues);
+                return res.status(201).json(task.dataValues);
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+    }
+    catch (err) {
+        ErrorHandler(err, res, "AUDITORIUMS_TYPES", "POST");
+    }
+});
+
+
+app.post('/api/auditoriums', async (req, res) => {
+    try {
+        const payload = req.body;
+        console.log(payload);
+
+        PayloadChecker(payload, res);
+
+        const { auditorium, auditorium_name, auditorium_capacity, auditorium_type } = payload;
+        if (!auditorium || typeof (auditorium) != 'string' || !auditorium_name || typeof (auditorium_name) != 'string' || !auditorium_capacity || typeof (auditorium_capacity) != 'number' || !auditorium_type || typeof (auditorium_type) != 'string') {
+            return res.status(400).json({ error: "Bad request. Payload fields were invalid" });
+        }
+
+        orm.Auditorium.create({ auditorium: auditorium, auditorium_name: auditorium_name, auditorium_capacity: auditorium_capacity, auditorium_type: auditorium_type })
+            .then(task => {
+                console.log(task.dataValues);
+                return res.status(201).json(task.dataValues);
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+    }
+    catch (err) {
+        ErrorHandler(err, res, "AUDITORIUM", "POST");
+    }
+});
+
+app.put('/api/faculties', async (req, res) => {
+    try {
+        const payload = req.body;
+
+        PayloadChecker(payload, res);
+
+        const { faculty, faculty_name } = payload;
+        if (!faculty || typeof (faculty) != 'string' || !faculty_name || typeof (faculty_name) != 'string') {
+            return res.status(400).json({ error: "Bad request. Payload fields were invalid" });
+        }
+
+        await orm.Faculty.update(
+            { faculty_name: faculty_name },
+            { where: { faculty: faculty } }
+        )
+            .then(task => {
+                console.log("Result: ", task);
+                return res.status(200).json({ result: `Operation succeeded with code ${task}` });
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+    }
+    catch (err) {
+        ErrorHandler(err, res, "FACULTIES", "PUT");
+    }
+});
+
+app.put('/api/pulpits', async (req, res) => {
+    try {
+        const payload = req.body;
+
+        PayloadChecker(payload, res);
+
+        const { pulpit, pulpit_name, faculty } = payload;
+        if (!pulpit || typeof (pulpit) != 'string' || !pulpit_name || typeof (pulpit_name) != 'string' || !faculty || typeof (faculty) != 'string') {
+            return res.status(400).json({ error: "Bad request. Payload fields were invalid" });
+        }
+
+        await orm.Pulpit.update(
+            { pulpit_name: pulpit_name, faculty: faculty },
+            { where: { pulpit: pulpit } }
+        )
+            .then(task => {
+                console.log("Result: ", task);
+                return res.status(200).json({ result: `Operation succeded with code ${task}` });
+            })
+            .catch(err => {
+                throw new Error(err);
+            });
+    }
+    catch (err) {
+        ErrorHandler(err, res, "PULPITS", "PUT");
+    }
+
+});
+
+
+app.put('/api/subjects', async (req, res) => {
+    try {
+        const payload = req.body;
+
+        PayloadChecker(payload, res);
+
+        const { subject, subject_name, pulpit } = payload;
+        if (!subject || typeof (subject) != 'string' || !subject_name || typeof (subject_name) != 'string' || !pulpit || typeof (pulpit) != 'string') {
+            return res.status(400).json({ error: "Bad request. Payload fields were invalid" });
+        }
+
+        await orm.Subject.update(
+            { subject_name: subject_name, pulpit: pulpit },
+            { where: { subject: subject } }
+        )
+            .then(task => {
+                console.log("Result: ", task);
+
+                orm.Subject.findAll({
+                    where: { subject: subject }
+                })
+                .then(result=>{
+                    console.log("Data result: ",result);
+                    return res.status(200).json(result);
+                })
+                .catch(err=>{
+                    throw new Error(err);
+                });
+
+            })
+            .catch(err=>{
+                throw new Error(err);
+            });
+    }
+    catch (err) {
+        ErrorHandler(err, res, "SUBJECTS", "PUT");
+    }
+});
+
+app.put('/api/auditoriumstypes',async(req,res)=>{
     try{
-        
+        const payload = req.body;
+
+        PayloadChecker(payload,res);
+
+        const {auditorium_type,auditorium_type_name} = payload;
+        if(!auditorium_type||typeof(auditorium_type)!='string'||!auditorium_type_name||typeof(auditorium_type_name)!='string'){
+            return res.status(400).json({error:"Bad request. Payload fields were invalid"});
+        }
+
+        await orm.Auditorium_type.update(
+            {auditorium_type_name:auditorium_type_name},
+            {where:{auditorium_type:auditorium_type}}
+        )
+        .then(task=>{
+            console.log("Result: ",task);
+
+            orm.Auditorium_type.findAll({
+                where:{auditorium_type:auditorium_type}
+            })
+            .then(result=>{
+                console.log("Data result: ",result);
+                return res.status(200).json(result);
+            })
+            .catch(err=>{
+                throw new Error(err);
+            });
+        })
+        .catch(err=>{
+            throw new Error(err);
+        });
     }
     catch(err){
-        console.error(err);
+        ErrorHandler(err,res,"AUDITORIUMS_TYPES","PUT");
     }
 });
 
+app.put('/api/auditoriums',async(req,res)=>{
+    try{
+        const payload = req.body;
+
+        PayloadChecker(payload,res);
+
+        const {}
+    }
+    catch(err){
+        ErrorHandler(err,res,"AUDITORIUMS","PUT");
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
--- CPSAP/Code/Lab_18/TablesCreation.sql
+++ CPSAP/Code/Lab_18/TablesCreation.sql
@@ -1,38 +1,44 @@
 use UNIVER;
 
 create table FACULTY(
-	FACULTY varchar(20) primary key,
-	FACULTY_NAME varchar(50) not null
+	FACULTY nvarchar(20) primary key,
+	FACULTY_NAME nvarchar(100) not null
 );
+
 
 create table PULPIT(
-	PULPIT varchar(20) primary key,
-	PULPIT_NAME varchar(20) not null,
-	FACULTY varchar(20) references FACULTY(FACULTY)
+	PULPIT nvarchar(20) primary key,
+	PULPIT_NAME nvarchar(100) not null,
+	FACULTY nvarchar(20) references FACULTY(FACULTY)
 );
 
+
 create table TEACHER(
-	TEACHER varchar(20) primary key,
-	TEACHER_NAME varchar(50) not null,
-	PULPIT varchar(20)  references PULPIT(PULPIT)
+	TEACHER nvarchar(20) primary key,
+	TEACHER_NAME nvarchar(100) not null,
+	PULPIT nvarchar(20)  references PULPIT(PULPIT)
 );
+
 
 
 create table SUBJECT(
-	SUBJECT varchar(20) primary key,
-	SUBJECT_NAME varchar(50) not null,
-	PULPIT varchar(20) references PULPIT(PULPIT)
+	SUBJECT nvarchar(20) primary key,
+	SUBJECT_NAME nvarchar(150) not null,
+	PULPIT nvarchar(20) references PULPIT(PULPIT)
 );
+
 
 
 create table AUDITORIUM_TYPE(
-	AUDITORIUM_TYPE varchar(20) primary key,
-	AUDITORIUM_TYPE_NAME varchar(50) not null
+	AUDITORIUM_TYPE nvarchar(20) primary key,
+	AUDITORIUM_TYPE_NAME nvarchar(50) not null
 );
 
+
+
 create table AUDITORIUM(
-	AUDITORIUM varchar(20) primary key,
-	AUDITORIUM_NAME varchar(50) not null,
+	AUDITORIUM nvarchar(20) primary key,
+	AUDITORIUM_NAME nvarchar(100) not null,
 	AUDITORIUM_CAPACITY int not null,
-	AUDITORIUM_TYPE varchar(20) references AUDITORIUM_TYPE(AUDITORIUM_TYPE)
+	AUDITORIUM_TYPE nvarchar(20) references AUDITORIUM_TYPE(AUDITORIUM_TYPE)
 );
