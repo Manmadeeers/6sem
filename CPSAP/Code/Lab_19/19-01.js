363464f608c80b110bee6e8eb3b0dd378935a9cc
Manmad <philmade6@gmail.com>
2026-02-24 12:46:59 +0300

    CPSAP:lab19 in progress

--- CPSAP/Code/Lab_19/19-01.js
+++ CPSAP/Code/Lab_19/19-01.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 const studentRoutes = require('./routes/StudentsRouter.js');
 
 const app = express();
@@ -8,16 +9,17 @@ const PORT = 3000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-const methodOverride = require('method-override');
+
 app.use(methodOverride('_method'));
 
 app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'public'));
+app.set('view','./views');
 
 
-app.
+app.use(express.static(path.join(__dirname,'public/css')));
+app.use('/students',studentRoutes);
 
-
+app.get('/test',(req,res)=>{res.send("OK")});
 
 app.listen(PORT, () => {
     console.log(`Server listening http://localhost:${PORT}`);
