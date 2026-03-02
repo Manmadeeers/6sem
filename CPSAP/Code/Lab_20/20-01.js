cf2683d3f7cfbbf9b5150af69568124b391fadbd
Manmad <philmade6@gmail.com>
2026-03-02 12:40:03 +0300

    CPSAP

--- CPSAP/Code/Lab_20/20-01.js
+++ CPSAP/Code/Lab_20/20-01.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const hbs = require('express-handlebars').create({extname:'.hbs'});
+const path = require('path');
 const PORT = 3000;
 const app = express();
 
+
+app.set(express.static(path.join(__dirname,'public')));
+
+app.set('.hbs',hbs.engine);
+app.set('view engine','.hbs');
+
+app.get('/',(req,res)=>{
+    res.render('index',{layout:null,hw:"Hello World"});
+});
+
 app.listen(PORT,()=>{
-    console.log("Server running at http://")
+    console.log(`Server running at http://localhost:${PORT}`);
 });
